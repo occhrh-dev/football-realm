991f68d8783ed642f27fc001d69c3217d048c8f9
occhrh-dev <occ.hrh@gmail.com>
2026-06-12 19:59:06 +0700

    v5.1: duel FX under cards, slower rolls, ball animation, in-box next button, stamina balance

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'football-realm-v5';
+const CACHE = 'football-realm-v6';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
